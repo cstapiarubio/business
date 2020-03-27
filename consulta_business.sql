b9831edb5e76ab28daffc427bf3876b08fe629ea
carolina_tapia <cstapiarubio@gmail.com>
2020-03-26 23:47:33 -0300

    consulta 6: recuento de leads para cada cliente

--- consulta_business.sql
+++ consulta_business.sql
@@ -25,16 +25,22 @@ where client_id = 1
 group by año;*/
 
 /*5. ¿Qué consulta ejecutaría para obtener el número total de clientes potenciales generados para cada uno de los sitios 
-entre el 1 de enero de 2011 y el 15 de febrero de 2011?*/
+entre el 1 de enero de 2011 y el 15 de febrero de 2011?
 select count(leads_id) as leads, site_id
 from leads
  where registered_datetime >= '2011/01/01' AND registered_datetime < '2011/02/15'
- group by site_id;
+ group by site_id;*/
  
-
-/*billingleads6. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes y el número total de clientes potenciales que hemos generado para cada uno de nuestros clientes entre el 1 de enero de 2011 y el 31 de diciembre de 2011?
-
-7. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes y el número total de clientes potenciales que hemos generado para cada cliente cada mes entre los meses 1 y 6 del año 2011?
+/*6. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes y el número total de clientes potenciales que 
+hemos generado para cada uno de nuestros clientes entre el 1 de enero de 2011 y el 31 de diciembre de 2011?*/
+select clients.first_name, clients.last_name, count(leads.leads_id) as leads
+from clients 
+inner join sites
+on clients.client_id = sites.client_id
+inner join leads
+on sites.site_id = leads.site_id
+group by clients.client_id;
+/*7. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes y el número total de clientes potenciales que hemos generado para cada cliente cada mes entre los meses 1 y 6 del año 2011?
 
 8. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes y el número total de clientes potenciales que hemos generado para cada uno de los sitios de nuestros clientes entre el 1 de enero de 2011 y el 31 de diciembre de 2011? Solicite esta consulta por ID de cliente. Presente una segunda consulta que muestre todos los clientes, los nombres del sitio y el número total de clientes potenciales generados en cada sitio en todo momento.
 

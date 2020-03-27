09b3209632fb96811e338a0842aafd349cecbf58
carolina_tapia <cstapiarubio@gmail.com>
2020-03-26 23:31:14 -0300

    consulta 5: clientes potenciales por sitio entre enero a feb de 2011

--- consulta_business.sql
+++ consulta_business.sql
@@ -13,24 +13,26 @@ where client_id =2;
 
 select domain_name, client_id
 from sites
-where client_id =10;*/
+where client_id =10;
 
-/*4. ¿Qué consulta ejecutaría para obtener el número total de sitios creados por mes por año para el cliente con una 
+4. ¿Qué consulta ejecutaría para obtener el número total de sitios creados por mes por año para el cliente con una 
 identificación de 1? 
-¿Qué pasa con el cliente = 20?*/
+¿Qué pasa con el cliente = 20?
 
 select year (created_datetime) as año, count(created_datetime) as sitios, client_id
 from sites
 where client_id = 1 
-group by año;
+group by año;*/
 
+/*5. ¿Qué consulta ejecutaría para obtener el número total de clientes potenciales generados para cada uno de los sitios 
+entre el 1 de enero de 2011 y el 15 de febrero de 2011?*/
+select count(leads_id) as leads, site_id
+from leads
+ where registered_datetime >= '2011/01/01' AND registered_datetime < '2011/02/15'
+ group by site_id;
+ 
 
-/*group by count(created_datetime); /*and created_datetime like ('%2010%');
-/*where created_datetime like ('%2011%') and client_id =1;*/
-
-/*5. ¿Qué consulta ejecutaría para obtener el número total de clientes potenciales generados para cada uno de los sitios entre el 1 de enero de 2011 y el 15 de febrero de 2011?
-
-6. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes y el número total de clientes potenciales que hemos generado para cada uno de nuestros clientes entre el 1 de enero de 2011 y el 31 de diciembre de 2011?
+/*billingleads6. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes y el número total de clientes potenciales que hemos generado para cada uno de nuestros clientes entre el 1 de enero de 2011 y el 31 de diciembre de 2011?
 
 7. ¿Qué consulta ejecutaría para obtener una lista de nombres de clientes y el número total de clientes potenciales que hemos generado para cada cliente cada mes entre los meses 1 y 6 del año 2011?
 
